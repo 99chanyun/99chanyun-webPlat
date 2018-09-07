422ae2a1fbf8a0e912f33c65a8dd5c09dc209c42
yjsn1990 <liuyang_sh@shanlinjinrong.com>
2018-09-07 16:14:53 +0800

    init

---
+++ src/main/java/com/chanyun/controller/AppController.java
@@ -0,0 +1,22 @@
+package com.chanyun.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+@RequestMapping("/")
+public class AppController {
+
+	@RequestMapping("/test")
+	@ResponseBody
+	public String test(){
+		return "hello word";
+	}
+	
+	@RequestMapping("/")
+	public String index(){
+		return "index";
+	}
+}
